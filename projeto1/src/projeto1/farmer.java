2aa81aa261845ba447ceb05a336b6034cd16c7a3
Tiago Ribau Rodrigues <trodrigues@ua.pt>
2020-03-03 15:32:55 +0000

    setting up some oop structure

---
+++ projeto1/src/projeto1/farmer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto1;
+
+/**
+ *
+ * @author TiagoRodrigues
+ */
+public class farmer {
+    int max_steps;
+    int timeout;
+    int corn_cobs;
+    int position;
+        
+    public farmer(int max_steps, int timeout, int corn_cobs){
+        this.max_steps = max_steps;
+        this.timeout = timeout;
+        this.corn_cobs = corn_cobs;
+    }
+}
---
+++ projeto1/src/projeto1/main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto1;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author TiagoRodrigues
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int number_farmers = 0;
+        
+        ArrayList<String> storehouse = new ArrayList<>();
+        ArrayList<String> standing_area = new ArrayList<>();
+        // decidir a estrutura de dados a utilizar para o caminho
+        ArrayList<String> granary = new ArrayList<>();
+        
+        
+        
+    }    
+}
+
+
