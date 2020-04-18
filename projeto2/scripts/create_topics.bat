b8243cc2e01d9917783d1ed5ae8a24bf37a02653
Tiago Ribau Rodrigues <trodrigues@ua.pt>
2020-04-18 19:02:18 +0100

    scripts + read me

---
+++ projeto2/scripts/create_topics.bat
@@ -0,0 +1,3 @@
+start bin\windows\kafka-topics.bat --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic BatchTopic && bin\windows\kafka-topics.bat --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic AlarmTopic && bin\windows\kafka-topics.bat --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ReportTopic
+
+exit 0
